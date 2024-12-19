5cc0decfb2fc7cb83158c3c9f9ce251bd92b8fc6
Rui Barreiro <rui.fonseca.barreiro@fidelidade.pt>
2024-12-19 19:14:28 +0000

    added /newapp

--- appserver/backend/index.js
+++ appserver/backend/index.js
@@ -1,15 +1,49 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const Joi = require('joi');
+const app = express();
+const port = 3000;
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
+const r = require('rethinkdb');
+
+var connection = null;
+r.connect( {host: 'rethinkdb', port: 28015}, function(err, conn) {
+    if (err) throw err;
+    r.branch(
+        r.db_list().contains("appserver").not(), 
+        [
+            r.db_create("appserver"),
+            r.db("appserver").table_create("apps")
+        ]
+    ).run(conn, (err, res)=>{
+        if (err) throw err;
+        console.log(res)
+    })
+    connection = conn;
+});
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    r.db("appserver").table("apps").pluck("name").coerceTo('array').run(conn, (err, res)=>{
+        if (err) throw err;
+        res.send(res)
+    })
 })
+
+const newappSchema = Joi.object({name : Joi.string().required(), "def" : Joi.string().required()});
+
+app.put('/newapp', (req, res) => {
+    const { error, value } = newappSchema.validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    r.db('appserver').table("apps").insert({id : value.name, def : value.def})
+});
+
 
 app.listen(port, () => {
   console.log(`appserver listening on port ${port}`)
-})
+});
