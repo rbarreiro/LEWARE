9c5af3c157973885eb67d92f13846701428464b9
Rui Barreiro <rui.fonseca.barreiro@fidelidade.pt>
2024-12-19 13:21:22 +0000

    fix swagger file name

--- appserver/backend/index.js
+++ appserver/backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerDocument = require('./swagger-output.json');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
