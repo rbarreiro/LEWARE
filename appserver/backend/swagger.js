a674e8dff9314f5e23b1c7e14e70a01eb4b1efba
Rui Barreiro <rui.fonseca.barreiro@fidelidade.pt>
2024-12-19 13:19:05 +0000

    fix swagger

--- appserver/backend/swagger.js
+++ appserver/backend/swagger.js
@@ -3,4 +3,11 @@ const swaggerAutogen = require('swagger-autogen')()
 const outputFile = './swagger-output.json';
 const routes = ['./index.js'];
 
+const doc = {
+  info: {
+    title: 'App Server',
+  },
+  host: 'localhost:3000'
+};
+
 swaggerAutogen(outputFile, routes, doc);
